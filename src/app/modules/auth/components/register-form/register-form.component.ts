3e5938648d9dfc036696386f4a4f0960287d52cc
Adrianh De Lucio <134009848+adrianhCoder@users.noreply.github.com>
2023-07-27 18:49:43 -0500

    Register works correctly!

--- src/app/modules/auth/components/register-form/register-form.component.ts
+++ src/app/modules/auth/components/register-form/register-form.component.ts
@@ -5,6 +5,9 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 import { CustomValidators } from '@utils/validators';
 
+import {RequestStatus} from '@models/request-status.model'
+import {AuthService} from '@services/auth.service'
+
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
@@ -13,25 +16,41 @@ export class RegisterFormComponent {
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.minLength(6), Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]],
     confirmPassword: ['', [Validators.required]],
   }, {
     validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
   });
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService : AuthService
   ) {}
 
   register() {
     if (this.form.valid) {
       this.status = 'loading';
       const { name, email, password } = this.form.getRawValue();
+      this.authService.register(name,password,email)
+      .subscribe({
+        next: () =>{
+          this.status = 'success';
+          this.router.navigate(['/login'])
+        },
+        error: (error) =>{
+          this.status = 'failed';
+          console.log(error);
+          //if (error.error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
+           // this.message = 'This user already exists. Do you want to Login?';
+       //   }
+
+        }
+      })
       console.log(name, email, password);
     } else {
       this.form.markAllAsTouched();
